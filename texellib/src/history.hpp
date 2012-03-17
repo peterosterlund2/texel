e01e9b119144887227f78a483cc88f409a8813d6
Peter Österlund <peterosterlund2@gmail.com>
2012-03-17 07:41:12 +0000

    Texel: Made the history heuristic data structure a little more cache friendly.
    git-svn-id: file:///home/petero/vc/svnrepos/texel/trunk@48 4abc0a36-5a1e-4bec-8e7d-414f7125f69d

--- texellib/src/history.hpp
+++ texellib/src/history.hpp
@@ -16,17 +16,21 @@
  */
 class History {
 private:
-    int countSuccess[Piece::nPieceTypes][64];
-    int countFail[Piece::nPieceTypes][64];
-    mutable int score[Piece::nPieceTypes][64];
+    struct Entry {
+        int countSuccess;
+        int countFail;
+        mutable int score;
+    };
+    struct Entry ht[Piece::nPieceTypes][64];
 
 public:
     History() {
         for (int p = 0; p < Piece::nPieceTypes; p++) {
             for (int sq = 0; sq < 64; sq++) {
-                countSuccess[p][sq] = 0;
-                countFail[p][sq] = 0;
-                score[p][sq] = -1;
+                Entry& e = ht[p][sq];
+                e.countSuccess = 0;
+                e.countFail = 0;
+                e.score = -1;
             }
         }
     }
@@ -35,36 +39,39 @@ public:
     void addSuccess(const Position& pos, const Move& m, int depth) {
         int p = pos.getPiece(m.from());
         int cnt = depth;
-        int val = countSuccess[p][m.to()] + cnt;
+        Entry& e = ht[p][m.to()];
+        int val = e.countSuccess + cnt;
         if (val > 1000) {
             val /= 2;
-            countFail[p][m.to()] /= 2;
+            e.countFail /= 2;
         }
-        countSuccess[p][m.to()] = val;
-        score[p][m.to()] = -1;
+        e.countSuccess = val;
+        e.score = -1;
     }
 
     /** Record move as a failure. */
     void addFail(const Position& pos, const Move& m, int depth) {
         int p = pos.getPiece(m.from());
         int cnt = depth;
-        countFail[p][m.to()] += cnt;
-        score[p][m.to()] = -1;
+        Entry& e = ht[p][m.to()];
+        e.countFail += cnt;
+        e.score = -1;
     }
 
     /** Get a score between 0 and 49, depending of the success/fail ratio of the move. */
     int getHistScore(const Position& pos, const Move& m) const {
         int p = pos.getPiece(m.from());
-        int ret = score[p][m.to()];
+        const Entry& e = ht[p][m.to()];
+        int ret = e.score;
         if (ret >= 0)
             return ret;
-        int succ = countSuccess[p][m.to()];
-        int fail = countFail[p][m.to()];
+        int succ = e.countSuccess;
+        int fail = e.countFail;
         if (succ + fail > 0) {
             ret = succ * 49 / (succ + fail);
         } else
             ret = 0;
-        score[p][m.to()] = ret;
+        e.score = ret;
         return ret;
     }
 };
