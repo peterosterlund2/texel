04be06a358da80597b3f07b6b8496a170512c35b
Peter Österlund <peterosterlund2@gmail.com>
2012-03-18 00:24:22 +0000

    Texel: Added GPLv3 copyright headers to all files.
    git-svn-id: file:///home/petero/vc/svnrepos/texel/trunk@50 4abc0a36-5a1e-4bec-8e7d-414f7125f69d

--- texellib/src/computerPlayer.cpp
+++ texellib/src/computerPlayer.cpp
@@ -1,4 +1,22 @@
 /*
+    Texel - A UCI chess engine.
+    Copyright (C) 2012  Peter Österlund, peterosterlund2@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
  * computerPlayer.cpp
  *
  *  Created on: Feb 25, 2012
@@ -17,7 +35,7 @@ static StaticInitializer<ComputerPlayer> cpInit;
 
 void
 ComputerPlayer::staticInitialize() {
-    std::string name = "Texel 1.01a3";
+    std::string name = "Texel 1.01a4";
     if (sizeof(char*) == 4)
         name += " 32-bit";
     if (sizeof(char*) == 8)
