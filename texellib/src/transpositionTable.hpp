e54e93099ef3e1399a9332861de0466a5f0f4a50
Peter Österlund <peterosterlund2@gmail.com>
2015-01-02 10:24:12 +0000

    Texel: Cosmetic changes, separation of interface from implementation.
    git-svn-id: file:///home/petero/vc/svnrepos/texel/trunk@477 4abc0a36-5a1e-4bec-8e7d-414f7125f69d

--- texellib/src/transpositionTable.hpp
+++ texellib/src/transpositionTable.hpp
@@ -40,112 +40,64 @@ class Position;
  */
 class TranspositionTable {
 public:
+    /** In-memory representation of TT entry. Uses std::atomic for thread safety,
+     * but accessed using memory_order_relaxed for maximum performance. */
     struct TTEntryStorage {
         std::atomic<U64> key;
         std::atomic<U64> data;
-        TTEntryStorage() {
-            key.store(0, std::memory_order_relaxed);
-            data.store(0, std::memory_order_relaxed);
-        }
-        TTEntryStorage(const TTEntryStorage& a) {
-            key.store(a.key.load(std::memory_order_relaxed), std::memory_order_relaxed);
-            data.store(a.data.load(std::memory_order_relaxed), std::memory_order_relaxed);
-        }
+        TTEntryStorage();
+        TTEntryStorage(const TTEntryStorage& a);
     };
     static_assert(sizeof(TTEntryStorage) == 16, "TTEntryStorage size wrong");
 
+    /** A local copy of a transposition table entry. */
     class TTEntry {
     public:
         /** Set type to T_EMPTY. */
-        void clear() {
-            key = 0;
-            data = 0;
-            static_assert(TType::T_EMPTY == 0, "type not set to T_EMPTY");
-        }
+        void clear();
 
         /** Store in transposition table, encoded for thread safety. */
-        void store(TTEntryStorage& ent) {
-            ent.key.store(key ^ data, std::memory_order_relaxed);
-            ent.data.store(data, std::memory_order_relaxed);
-        }
+        void store(TTEntryStorage& ent);
 
         /** Load from transposition table, decode the thread safety encoding. */
-        void load(const TTEntryStorage& ent) {
-            key = ent.key.load(std::memory_order_relaxed);
-            data = ent.data.load(std::memory_order_relaxed);
-            key ^= data;
-        }
+        void load(const TTEntryStorage& ent);
 
         /** Return true if this object is more valuable than the other, false otherwise. */
-        bool betterThan(const TTEntry& other, int currGen) const {
-            if ((getGeneration() == currGen) != (other.getGeneration() == currGen))
-                return getGeneration() == currGen;   // Old entries are less valuable
-            if ((getType() == TType::T_EXACT) != (other.getType() == TType::T_EXACT))
-                return getType() == TType::T_EXACT;         // Exact score more valuable than lower/upper bound
-            if (getDepth() != other.getDepth())
-                return getDepth() > other.getDepth();     // Larger depth is more valuable
-            return false;   // Otherwise, pretty much equally valuable
-        }
+        bool betterThan(const TTEntry& other, int currGen) const;
 
-        U64 getKey() const { return key; }
-        void setKey(U64 k) { key = k; }
+        U64 getKey() const;
+        void setKey(U64 k);
 
-        void getMove(Move& m) const {
-            int move = getBits(0, 16);
-            m.setMove(move & 63, (move >> 6) & 63, (move >> 12) & 15, m.score());
-        }
+        void getMove(Move& m) const;
 
-        void setMove(const Move& m) {
-            int move = (short)(m.from() + (m.to() << 6) + (m.promoteTo() << 12));
-            setBits(0, 16, move);
-        }
+        void setMove(const Move& m);
 
         /** Get the score from the hash entry and convert from "mate in x" to "mate at ply". */
-        int getScore(int ply) const {
-            int sc = (S16)getBits(16, 16);
-            if (SearchConst::isWinScore(sc))
-                sc -= ply;
-            else if (SearchConst::isLoseScore(sc))
-                sc += ply;
-            return sc;
-        }
+        int getScore(int ply) const;
 
         /** Convert score from "mate at ply" to "mate in x" and store in hash entry. */
-        void setScore(int score, int ply) {
-            if (SearchConst::isWinScore(score))
-                score += ply;
-            else if (SearchConst::isLoseScore(score))
-                score -= ply;
-            setBits(16, 16, score);
-        }
+        void setScore(int score, int ply);
 
-        int getDepth() const { return getBits(32, 10); }
-        void setDepth(int d) { setBits(32, 10, d); }
-        int getGeneration() const { return getBits(42, 4); }
-        void setGeneration(int g) { setBits(42, 4, g); }
-        int getType() const { return getBits(46, 2); }
-        void setType(int t) { setBits(46, 2, t); }
-        int getEvalScore() const { return (S16)getBits(48, 16); }
-        void setEvalScore(int s) { setBits(48, 16, s); }
+        int getDepth() const;
+        void setDepth(int d);
+        int getGeneration() const;
+        void setGeneration(int g);
+        int getType() const;
+        void setType(int t);
+        int getEvalScore() const;
+        void setEvalScore(int s);
 
     private:
         U64 key;        //  0 64 key         Zobrist hash key
-        U64 data;       //  0 16 move        from + (to<<6) ++ (promote<<12)
+        U64 data;       //  0 16 move        from + (to<<6) + (promote<<12)
                         // 16 16 score       Score from search
                         // 32 10 depth       Search depth
                         // 42  4 generation  Increase when OTB position changes
                         // 46  2 type        exact score, lower bound, upper bound
                         // 48 16 evalScore   Score from static evaluation
 
-        void setBits(int first, int size, unsigned int value) {
-            U64 mask = ((1ULL << size) - 1) << first;
-            data = (data & ~mask) | (((U64)value << first) & mask);
-        }
-
-        unsigned int getBits(int first, int size) const {
-            U64 sizeMask = ((1ULL << size) - 1);
-            return (data >> first) & sizeMask;
-        }
+        void setBits(int first, int size, unsigned int value);
+        unsigned int getBits(int first, int size) const;
     };
 
     /** Constructor. Creates an empty transposition table with numEntries slots. */
@@ -191,6 +143,145 @@ private:
     vector_aligned<TTEntryStorage> table;
     U8 generation;
 };
+
+
+inline
+TranspositionTable::TTEntryStorage::TTEntryStorage() {
+    key.store(0, std::memory_order_relaxed);
+    data.store(0, std::memory_order_relaxed);
+}
+
+inline
+TranspositionTable::TTEntryStorage::TTEntryStorage(const TTEntryStorage& a) {
+    key.store(a.key.load(std::memory_order_relaxed), std::memory_order_relaxed);
+    data.store(a.data.load(std::memory_order_relaxed), std::memory_order_relaxed);
+}
+
+
+inline void
+TranspositionTable::TTEntry::clear() {
+    key = 0;
+    data = 0;
+    static_assert(TType::T_EMPTY == 0, "type not set to T_EMPTY");
+}
+
+inline void
+TranspositionTable::TTEntry::store(TTEntryStorage& ent) {
+    ent.key.store(key ^ data, std::memory_order_relaxed);
+    ent.data.store(data, std::memory_order_relaxed);
+}
+
+inline void
+TranspositionTable::TTEntry::load(const TTEntryStorage& ent) {
+    key = ent.key.load(std::memory_order_relaxed);
+    data = ent.data.load(std::memory_order_relaxed);
+    key ^= data;
+}
+
+inline bool
+TranspositionTable::TTEntry::betterThan(const TTEntry& other, int currGen) const {
+    if ((getGeneration() == currGen) != (other.getGeneration() == currGen))
+        return getGeneration() == currGen;   // Old entries are less valuable
+    if ((getType() == TType::T_EXACT) != (other.getType() == TType::T_EXACT))
+        return getType() == TType::T_EXACT;         // Exact score more valuable than lower/upper bound
+    if (getDepth() != other.getDepth())
+        return getDepth() > other.getDepth();     // Larger depth is more valuable
+    return false;   // Otherwise, pretty much equally valuable
+}
+
+inline U64
+TranspositionTable::TTEntry::getKey() const {
+    return key;
+}
+
+inline void
+TranspositionTable::TTEntry::setKey(U64 k) {
+    key = k;
+}
+
+inline void
+TranspositionTable::TTEntry::getMove(Move& m) const {
+    int move = getBits(0, 16);
+    m.setMove(move & 63, (move >> 6) & 63, (move >> 12) & 15, m.score());
+}
+
+inline void
+TranspositionTable::TTEntry::setMove(const Move& m) {
+    int move = (short)(m.from() + (m.to() << 6) + (m.promoteTo() << 12));
+    setBits(0, 16, move);
+}
+
+inline int
+TranspositionTable::TTEntry::getScore(int ply) const {
+    int sc = (S16)getBits(16, 16);
+    if (SearchConst::isWinScore(sc))
+        sc -= ply;
+    else if (SearchConst::isLoseScore(sc))
+        sc += ply;
+    return sc;
+}
+
+inline void
+TranspositionTable::TTEntry::setScore(int score, int ply) {
+    if (SearchConst::isWinScore(score))
+        score += ply;
+    else if (SearchConst::isLoseScore(score))
+        score -= ply;
+    setBits(16, 16, score);
+}
+
+inline int
+TranspositionTable::TTEntry::getDepth() const {
+    return getBits(32, 10);
+}
+
+inline void
+TranspositionTable::TTEntry::setDepth(int d) {
+    setBits(32, 10, d);
+}
+
+inline int
+TranspositionTable::TTEntry::getGeneration() const {
+    return getBits(42, 4);
+}
+
+inline void
+TranspositionTable::TTEntry::setGeneration(int g) {
+    setBits(42, 4, g);
+}
+
+inline int
+TranspositionTable::TTEntry::getType() const {
+    return getBits(46, 2);
+}
+
+inline void
+TranspositionTable::TTEntry::setType(int t) {
+    setBits(46, 2, t);
+}
+
+inline int
+TranspositionTable::TTEntry::getEvalScore() const {
+    return (S16)getBits(48, 16);
+}
+
+inline void
+TranspositionTable::TTEntry::setEvalScore(int s) {
+    setBits(48, 16, s);
+}
+
+inline void
+TranspositionTable::TTEntry::setBits(int first, int size, unsigned int value) {
+    U64 mask = ((1ULL << size) - 1) << first;
+    data = (data & ~mask) | (((U64)value << first) & mask);
+}
+
+inline unsigned int
+TranspositionTable::TTEntry::getBits(int first, int size) const {
+    U64 sizeMask = ((1ULL << size) - 1);
+    return (data >> first) & sizeMask;
+}
+
 
 inline size_t
 TranspositionTable::getIndex(U64 key) const {
