44e9d1fcf12019ecc898f223c3c29082477a9be1
Peter Österlund <peterosterlund2@gmail.com>
2015-01-23 23:56:08 +0000

    Texel: Changed version number to 1.05.
    git-svn-id: file:///home/petero/vc/svnrepos/texel/trunk@487 4abc0a36-5a1e-4bec-8e7d-414f7125f69d

--- texellib/src/computerPlayer.cpp
+++ texellib/src/computerPlayer.cpp
@@ -36,7 +36,7 @@ static StaticInitializer<ComputerPlayer> cpInit;
 
 void
 ComputerPlayer::staticInitialize() {
-    std::string name = "Texel 1.05a35";
+    std::string name = "Texel 1.05";
     if (sizeof(char*) == 4)
         name += " 32-bit";
     if (sizeof(char*) == 8)
