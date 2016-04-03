5d09c09f121d4ffc9d40b9f5e9fbee484ce7995d
Peter Österlund <peterosterlund2@gmail.com>
2016-04-03 19:46:37 +0000

    BookGui: Connected widget signals to code.
    git-svn-id: file:///home/petero/vc/svnrepos/texel/trunk@642 4abc0a36-5a1e-4bec-8e7d-414f7125f69d

--- bookgui/src/bookgui.cpp
+++ bookgui/src/bookgui.cpp
@@ -34,10 +34,10 @@ int main(int argc, char* argv[]) {
 }
 
 BookGui::BookGui(Glib::RefPtr<Gtk::Application> app0)
-    : app(app0), mainWindow(nullptr),
-      bbControl(*this),
+    : app(app0), mainWindow(nullptr), bbControl(*this),
       gameTree(std::cin), // FIXME!!
-      loadingBook(false), searching(false), analysing(false), bookDirty(false) {
+      loadingBook(false), searchState(SearchState::STOPPED),
+      analysing(false), bookDirty(false) {
     builder = Gtk::Builder::create();
     builder->add_from_resource("/main/bookgui_glade.xml");
 }
@@ -46,11 +46,18 @@ void
 BookGui::run() {
     builder->get_widget("mainWindow", mainWindow);
     connectSignals();
+    getWidgets();
     updateEnabledState();
     app->run(*mainWindow);
 }
 
 // --------------------------------------------------------------------------------
+
+void
+BookGui::getWidgets() {
+    builder->get_widget("analyzeScore", analyzeScore);
+    builder->get_widget("pvText", pvText);
+}
 
 void
 BookGui::connectSignals() {
@@ -73,6 +80,72 @@ BookGui::connectSignals() {
     builder->get_widget("quitMenuItem", quitItem);
     quitItem->signal_activate().connect([this]{ quit(); });
     mainWindow->signal_delete_event().connect(sigc::mem_fun(*this, &BookGui::deleteEvent));
+
+    // Settings
+    builder->get_widget("threads", threads);
+    threads->signal_value_changed().connect([this]{ threadsValueChanged(); });
+
+    builder->get_widget("compTime", compTime);
+    compTime->signal_value_changed().connect([this]{ compTimeChanged(); });
+
+    builder->get_widget("depthCost", depthCost);
+    depthCost->signal_value_changed().connect([this]{ depthCostChanged(); });
+
+    builder->get_widget("ownPathErrCost", ownPathErrCost);
+    ownPathErrCost->signal_value_changed().connect([this]{ ownPathErrCostChanged(); });
+
+    builder->get_widget("otherPathErrCost", otherPathErrCost);
+    otherPathErrCost->signal_value_changed().connect([this]{ otherPathErrCostChanged(); });
+
+    builder->get_widget("pgnMaxPly", pgnMaxPly);
+    pgnMaxPly->signal_value_changed().connect([this]{ pgnMaxPlyChanged(); });
+
+    // Start/stop buttons
+    builder->get_widget("startButton", startButton);
+    startButton->signal_clicked().connect([this]{ startSearch(); });
+
+    builder->get_widget("softStopButton", softStopButton);
+    softStopButton->signal_clicked().connect([this]{ softStopSearch(); });
+
+    builder->get_widget("hardStopButton", hardStopButton);
+    hardStopButton->signal_clicked().connect([this]{ hardStopSearch(); });
+
+    // Focus buttons
+    builder->get_widget("setFocusButton", setFocusButton);
+    setFocusButton->signal_clicked().connect([this]{ setFocus(); });
+
+    builder->get_widget("getFocusButton", getFocusButton);
+    getFocusButton->signal_clicked().connect([this]{ getFocus(); });
+
+    builder->get_widget("clearFocusButton", clearFocusButton);
+    clearFocusButton->signal_clicked().connect([this]{ clearFocus(); });
+
+    // PGN buttons
+    builder->get_widget("importPgnButton", importPgnButton);
+    importPgnButton->signal_clicked().connect([this]{ importPgn(); });
+
+    builder->get_widget("addPgnButton", addPgnButton);
+    addPgnButton->signal_clicked().connect([this]{ addPgn(); });
+
+    builder->get_widget("applyPgnButton", applyPgnButton);
+    applyPgnButton->signal_clicked().connect([this]{ applyPgn(); });
+
+    builder->get_widget("clearPgnButton", clearPgnButton);
+    clearPgnButton->signal_clicked().connect([this]{ clearPgn(); });
+
+    // Navigate buttons
+    builder->get_widget("backButton", backButton);
+    backButton->signal_clicked().connect([this]{ posGoBack(); });
+
+    builder->get_widget("forwardButton", forwardButton);
+    forwardButton->signal_clicked().connect([this]{ posGoForward(); });
+
+    // Analyze buttons
+    builder->get_widget("nextGenButton", nextGenButton);
+    nextGenButton->signal_clicked().connect([this]{ nextGeneration(); });
+
+    builder->get_widget("analyzeToggle", analyzeToggle);
+    analyzeToggle->signal_clicked().connect([this]{ toggleAnalyzeMode(); });
 }
 
 void
@@ -94,6 +167,8 @@ BookGui::bookStateChanged() {
         case BookBuildControl::Change::QUEUE:
             updateEnabled = true;
             updateQueueView();
+            if (bbControl.nRunningThreads() == 0)
+                searchState = SearchState::STOPPED;
             break;
         case BookBuildControl::Change::PV:
             updatePVView();
@@ -120,24 +195,69 @@ BookGui::updateQueueView() {
 
 void
 BookGui::updatePVView() {
+    int score = INT_MIN;
+    std::string pv;
+    if (analysing)
+        bbControl.getPV(score, pv);
+    analyzeScore->set_text(score == INT_MIN ? "" : num2Str(score));
+    pvText->get_buffer()->set_text(pv);
+}
+
+void
+BookGui::updatePGNView() {
 
 }
 
 void
 BookGui::updateEnabledState() {
-    bool builderIdle = bbControl.nRunningThreads() == 0 && !loadingBook;
+    // Menu items
+    bool builderIdle = searchState == SearchState::STOPPED &&
+                       bbControl.nRunningThreads() == 0 && !loadingBook;
     newItem->set_sensitive(builderIdle);
     openItem->set_sensitive(builderIdle);
     saveItem->set_sensitive(!bbControl.getBookFileName().empty() && !loadingBook);
     saveAsItem->set_sensitive(!loadingBook);
-    quitItem->set_sensitive(true);
+//  quitItem->set_sensitive(true);
+
+    // Settings widgets
+    bool searchStopped = searchState == SearchState::STOPPED;
+    threads->set_sensitive(searchStopped);
+//  compTime->set_sensitive(true);
+    depthCost->set_sensitive(searchStopped);
+    ownPathErrCost->set_sensitive(searchState == SearchState::STOPPED);
+    otherPathErrCost->set_sensitive(searchState == SearchState::STOPPED);
+//  pgnMaxPly->set_sensitive(true);
+
+    // Start/stop buttons
+    startButton->set_sensitive(searchState == SearchState::STOPPED);
+    softStopButton->set_sensitive(searchState == SearchState::RUNNING);
+    hardStopButton->set_sensitive(searchState != SearchState::STOPPED);
+
+    // Focus buttons
+//  setFocusButton->set_sensitive(true);
+//  getFocusButton->set_sensitive(true);
+//  clearFocusButton->set_sensitive(true);
+
+    // PGN buttons
+//  importPgnButton->set_sensitive(true);
+//  addPgnButton->set_sensitive(true);
+//  applyPgnButton->set_sensitive(true);
+//  clearPgnButton->set_sensitive(true);
+
+    // Navigate buttons
+    backButton->set_sensitive(!moves.empty());
+    forwardButton->set_sensitive(!nextMoves.empty());
+
+    // Analyze buttons
+//  nextGenButton->set_sensitive(true);
+//  analyzeToggle->set_sensitive(true);
 }
 
 // --------------------------------------------------------------------------------
 
 void
 BookGui::newBook() {
-    if (bbControl.nRunningThreads() > 0 || loadingBook)
+    if (searchState != SearchState::STOPPED || bbControl.nRunningThreads() > 0 || loadingBook)
         return;
     if (!askSaveIfDirty())
         return;
@@ -145,6 +265,7 @@ BookGui::newBook() {
     bbControl.newBook();
     pos = TextIO::readFEN(TextIO::startPosFEN);
     moves.clear();
+    nextMoves.clear();
     bookDirty = false;
     updateBoardAndTree();
     if (analysing)
@@ -154,12 +275,12 @@ BookGui::newBook() {
 
 void
 BookGui::openBookFile() {
-    if (bbControl.nRunningThreads() > 0 || loadingBook)
+    if (searchState != SearchState::STOPPED || bbControl.nRunningThreads() > 0 || loadingBook)
         return;
     if (!askSaveIfDirty())
         return;
 
-    Gtk::FileChooserDialog dialog("Open", Gtk::FILE_CHOOSER_ACTION_OPEN);
+    Gtk::FileChooserDialog dialog("Open book", Gtk::FILE_CHOOSER_ACTION_OPEN);
     dialog.set_transient_for(*mainWindow);
     dialog.add_button("_Cancel", Gtk::RESPONSE_CANCEL);
     dialog.add_button("_Open", Gtk::RESPONSE_OK);
@@ -288,3 +409,202 @@ BookGui::askSaveIfDirty() {
 }
 
 // --------------------------------------------------------------------------------
+
+void
+BookGui::threadsValueChanged() {
+    BookBuildControl::Params params;
+    bbControl.getParams(params);
+    params.nThreads = threads->get_value_as_int();
+    bbControl.setParams(params);
+}
+
+void
+BookGui::compTimeChanged() {
+    BookBuildControl::Params params;
+    bbControl.getParams(params);
+    params.computationTime = compTime->get_value_as_int();
+    bbControl.setParams(params);
+}
+
+void
+BookGui::depthCostChanged() {
+    BookBuildControl::Params params;
+    bbControl.getParams(params);
+    params.bookDepthCost = depthCost->get_value_as_int();
+    bbControl.setParams(params);
+}
+
+void
+BookGui::ownPathErrCostChanged() {
+    BookBuildControl::Params params;
+    bbControl.getParams(params);
+    params.ownPathErrorCost = ownPathErrCost->get_value_as_int();
+    bbControl.setParams(params);
+}
+
+void
+BookGui::otherPathErrCostChanged() {
+    BookBuildControl::Params params;
+    bbControl.getParams(params);
+    params.otherPathErrorCost = otherPathErrCost->get_value_as_int();
+    bbControl.setParams(params);
+}
+
+void
+BookGui::pgnMaxPlyChanged() {
+    pgnImportMaxPly = pgnMaxPly->get_value_as_int();
+}
+
+// --------------------------------------------------------------------------------
+
+void
+BookGui::startSearch() {
+    if (searchState != SearchState::STOPPED)
+        return;
+
+    bbControl.startSearch();
+    searchState = SearchState::RUNNING;
+    updateEnabledState();
+}
+
+void
+BookGui::softStopSearch() {
+    if (searchState != SearchState::RUNNING)
+        return;
+    bbControl.stopSearch(false);
+    searchState = SearchState::STOPPING;
+    updateEnabledState();
+}
+
+void
+BookGui::hardStopSearch() {
+    if (searchState == SearchState::STOPPED)
+        return;
+    bbControl.stopSearch(true);
+    searchState = SearchState::STOPPING;
+    updateEnabledState();
+}
+
+// --------------------------------------------------------------------------------
+
+void
+BookGui::setFocus() {
+    bbControl.setFocus(pos);
+}
+
+void
+BookGui::getFocus() {
+    bbControl.getFocus(pos);
+    moves.clear(); // FIXME!! Compute reasonable move path
+    nextMoves.clear(); // FIXME!! Compute reasonable move path by following "book PV"
+    updateBoardAndTree();
+}
+
+void
+BookGui::clearFocus() {
+    Position startPos = TextIO::readFEN(TextIO::startPosFEN);
+    bbControl.setFocus(startPos);
+}
+
+// --------------------------------------------------------------------------------
+
+void
+BookGui::importPgn() {
+    Gtk::FileChooserDialog dialog("Import PGN", Gtk::FILE_CHOOSER_ACTION_OPEN);
+    dialog.set_transient_for(*mainWindow);
+    dialog.add_button("_Cancel", Gtk::RESPONSE_CANCEL);
+    dialog.add_button("_Open", Gtk::RESPONSE_OK);
+
+    std::string filename = pgnImportFilename;
+    Glib::RefPtr<Gio::File> file = Gio::File::create_for_path(filename);
+    if (file && !filename.empty())
+        file = file->get_parent();
+    if (file)
+        dialog.set_current_folder_file(file);
+
+    auto bookFilter = Gtk::FileFilter::create();
+    bookFilter->set_name("PGN files");
+    bookFilter->add_pattern("*.pgn");
+    dialog.add_filter(bookFilter);
+    auto allFilter = Gtk::FileFilter::create();
+    allFilter->set_name("All files");
+    allFilter->add_pattern("*");
+    dialog.add_filter(allFilter);
+
+    if (dialog.run() != Gtk::RESPONSE_OK)
+        return;
+
+    pgnImportFilename = dialog.get_filename();
+
+    // FIXME!! Perform import
+
+    updatePGNView();
+}
+
+void
+BookGui::addPgn() {
+    // FIXME!!
+    updatePGNView();
+}
+
+void
+BookGui::applyPgn() {
+    // FIXME!!
+    updatePGNView();
+}
+
+void
+BookGui::clearPgn() {
+    // FIXME!!
+    updatePGNView();
+}
+
+// --------------------------------------------------------------------------------
+
+void
+BookGui::posGoBack() {
+    if (moves.empty())
+        return;
+
+    pos = TextIO::readFEN(TextIO::startPosFEN);
+    UndoInfo ui;
+    int N = moves.size();
+    for (int i = 0; i < N - 1; i++)
+        pos.makeMove(moves[i], ui);
+
+    nextMoves.insert(nextMoves.begin(), moves[N-1]);
+    moves.pop_back();
+
+    updateBoardAndTree();
+}
+
+void
+BookGui::posGoForward() {
+    if (nextMoves.empty())
+        return;
+
+    moves.push_back(nextMoves[0]);
+    UndoInfo ui;
+    pos.makeMove(nextMoves[0], ui);
+    nextMoves.erase(nextMoves.begin());
+
+    updateBoardAndTree();
+}
+
+// --------------------------------------------------------------------------------
+
+void
+BookGui::nextGeneration() {
+    bbControl.nextGeneration();
+}
+
+void
+BookGui::toggleAnalyzeMode() {
+    if (analyzeToggle->get_active()) {
+        bbControl.startAnalysis(pos);
+        analysing = true;
+    } else {
+        bbControl.stopAnalysis();
+        analysing = false;
+    }
+}
--- bookgui/src/bookgui.hpp
+++ bookgui/src/bookgui.hpp
@@ -39,6 +39,7 @@ public:
     void run();
 
 private:
+    void getWidgets();
     void connectSignals();
 
     /** Called by book builder worker thread when book state has changed. */
@@ -50,6 +51,7 @@ private:
     void updateBoardAndTree();
     void updateQueueView();
     void updatePVView();
+    void updatePGNView();
     void updateEnabledState();
 
     void newBook();
@@ -59,6 +61,32 @@ private:
     void quit();
     bool deleteEvent(_GdkEventAny* e);
     bool askSaveIfDirty(); // Return false if user canceled
+
+    void startSearch();
+    void softStopSearch();
+    void hardStopSearch();
+
+    void threadsValueChanged();
+    void compTimeChanged();
+    void depthCostChanged();
+    void ownPathErrCostChanged();
+    void otherPathErrCostChanged();
+    void pgnMaxPlyChanged();
+
+    void setFocus();
+    void getFocus();
+    void clearFocus();
+
+    void importPgn();
+    void addPgn();
+    void applyPgn();
+    void clearPgn();
+
+    void posGoBack();
+    void posGoForward();
+
+    void nextGeneration();
+    void toggleAnalyzeMode();
 
 
     Glib::RefPtr<Gtk::Application> app;
@@ -74,13 +102,60 @@ private:
     Gtk::MenuItem* saveAsItem = nullptr;
     Gtk::MenuItem* quitItem = nullptr;
 
+    // Settings widgets
+    Gtk::SpinButton* threads = nullptr;
+    Gtk::SpinButton* compTime = nullptr;
+    Gtk::SpinButton* depthCost = nullptr;
+    Gtk::SpinButton* ownPathErrCost = nullptr;
+    Gtk::SpinButton* otherPathErrCost = nullptr;
+    Gtk::SpinButton* pgnMaxPly = nullptr;
+
+    // Start/stop buttons
+    Gtk::Button* startButton = nullptr;
+    Gtk::Button* softStopButton = nullptr;
+    Gtk::Button* hardStopButton = nullptr;
+
+    // Focus buttons
+    Gtk::Button* setFocusButton = nullptr;
+    Gtk::Button* getFocusButton = nullptr;
+    Gtk::Button* clearFocusButton = nullptr;
+
+    // PGN buttons
+    Gtk::Button* importPgnButton = nullptr;
+    Gtk::Button* addPgnButton = nullptr;
+    Gtk::Button* applyPgnButton = nullptr;
+    Gtk::Button* clearPgnButton = nullptr;
+
+    // Navigate buttons
+    Gtk::Button* backButton = nullptr;
+    Gtk::Button* forwardButton = nullptr;
+
+    // Analyze buttons
+    Gtk::Button* nextGenButton = nullptr;
+    Gtk::ToggleButton* analyzeToggle = nullptr;
+
+    Gtk::Entry* analyzeScore = nullptr;
+    Gtk::TextView* pvText = nullptr;
+
+
     BookBuildControl bbControl;
     Position pos;            // Position corresponding to chess board and tree view.
     std::vector<Move> moves; // Moves leading to pos.
+    std::vector<Move> nextMoves; // Moves following pos.
     GameTree gameTree;       // Game tree corresponding to the PGN view.
-    BookBuildControl::Params searchParams; // Search parameters.
+
+    std::string pgnImportFilename; // Last filename for PGN import.
+    int pgnImportMaxPly = 40;
+
+
     bool loadingBook; // True when loading a book file.
-    bool searching;   // True when book building threads are running.
+
+    enum class SearchState {
+        RUNNING,      // Threads are running, new jobs are scheduled
+        STOPPING,     // Waiting for current jobs to finish
+        STOPPED       // Threads are not running
+    };
+    SearchState searchState;
     bool analysing;   // True when analysis thread is running.
     bool bookDirty;   // True if book is unsaved.
 };
