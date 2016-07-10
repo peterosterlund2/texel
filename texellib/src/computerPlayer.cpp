b913f4a7454a6a2689e9e9130fe2e62a63944c88
Peter Österlund <peterosterlund2@gmail.com>
2016-07-10 15:53:11 +0000

    Texel: Changed version number to 1.06.
    git-svn-id: file:///home/petero/vc/svnrepos/texel/trunk@695 4abc0a36-5a1e-4bec-8e7d-414f7125f69d

--- texellib/src/computerPlayer.cpp
+++ texellib/src/computerPlayer.cpp
@@ -36,7 +36,7 @@ static StaticInitializer<ComputerPlayer> cpInit;
 
 void
 ComputerPlayer::staticInitialize() {
-    std::string name = "Texel 1.06a52";
+    std::string name = "Texel 1.06";
     if (sizeof(char*) == 4)
         name += " 32-bit";
     engineName = name;
