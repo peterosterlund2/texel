9d3b6d8170f5184d7b1326682ac7a64a242af747
Peter Österlund <peterosterlund2@gmail.com>
2016-08-26 23:08:04 +0000

    Texel: Cosmetic changes.
    git-svn-id: file:///home/petero/vc/svnrepos/texel/trunk@719 4abc0a36-5a1e-4bec-8e7d-414f7125f69d

--- texellib/src/bitBoard.hpp
+++ texellib/src/bitBoard.hpp
@@ -214,7 +214,7 @@ BitBoard::firstSquare(U64 mask) {
 #ifdef HAS_CTZ
     if (sizeof(U64) == sizeof(long))
         return __builtin_ctzl(mask);
-    else if (sizeof(U64) == sizeof(long long))
+    else if (sizeof(U64) == 2*sizeof(long))
         return __builtin_ctzll(mask);
 #endif
     return trailingZ[(int)(((mask & -mask) * 0x07EDD5E59A4E28C2ULL) >> 58)];
@@ -232,7 +232,7 @@ BitBoard::bitCount(U64 mask) {
 #ifdef HAS_POPCNT
     if (sizeof(U64) == sizeof(long))
         return __builtin_popcountl(mask);
-    else if (sizeof(U64) == sizeof(long long))
+    else if (sizeof(U64) == 2*sizeof(long))
         return __builtin_popcountl(mask >> 32) +
                __builtin_popcountl(mask & 0xffffffffULL);
 #endif
