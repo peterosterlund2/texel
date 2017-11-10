peterosterlund2/texel

commit 71512d3d0542f81128788fd51d556b4c96b36207

Texel: Fixed rare crash when exiting the engine when running in cluster mode with more than 5 threads per node. (>5 threads means thread 0 has a grand  child on the same cluster node.)
git-svn-id: file:///home/petero/vc/svnrepos/texel/trunk@855 4abc0a36-5a1e-4bec-8e7d-414f7125f69d

texellib/src/parallel.cpp
@@ -197,12 +197,8 @@ Communicator::forwardStopAck() {
 void
 Communicator::sendQuitAck() {
     quitAckWaitChildren--;
-    if (hasQuitAck()) {
-        if (parent)
-            parent->doSendQuitAck();
-        else
-            notifyThread();
-    }
+    if (parent && hasQuitAck())
+        parent->doSendQuitAck();
 }
 
 void
@@ -558,7 +554,7 @@ ThreadCommunicator::notifyThread() {
 
 WorkerThread::WorkerThread(int threadNo, Communicator* parentComm,
                            int numWorkers, TranspositionTable& tt)
-    : threadNo(threadNo), numWorkers(numWorkers), terminate(false), tt(tt) {
+    : threadNo(threadNo), numWorkers(numWorkers), tt(tt) {
     if (parentComm) {
         auto f = [this,parentComm]() {
             mainLoop(parentComm, false);
@@ -569,7 +565,6 @@ WorkerThread::WorkerThread(int threadNo, Communicator* parentComm,
 
 WorkerThread::~WorkerThread() {
     children.clear();
-    terminate = true;
     threadNotifier.notify();
     if (thread)
         thread->join();
@@ -629,9 +624,8 @@ WorkerThread::mainLoop(Communicator* parentComm, bool cluster) {
     CommHandler handler(*this);
 
     while (true) {
-        threadNotifier.wait(Cluster::instance().isEnabled() ? 1 : -1);
-        if (terminate)
-            break;
+        bool handleClusterComm = Cluster::instance().isEnabled() && threadNo == 0;
+        threadNotifier.wait(handleClusterComm ? 1 : -1);
         comm->poll(handler);
         if (comm->hasQuitAck())
             break;
@@ -703,11 +697,7 @@ WorkerThread::CommHandler::setParam(const std::string& name, const std::string& 
 
 void
 WorkerThread::CommHandler::quit() {
-    if (wt.getThreadNo() == 0)
-        wt.comm->sendQuit();
-    else {
-        wt.comm->forwardQuitAck();
-    }
+    wt.comm->sendQuit();
 }
 
 void

texellib/src/parallel.hpp
@@ -401,7 +401,6 @@ private:
     const int numWorkers; // Number of worker threads including all child threads
 
     Notifier initialized;
-    std::atomic<bool> terminate;
 
     std::unique_ptr<Evaluate::EvalHashTables> et;
     std::unique_ptr<KillerTable> kt;
@@ -460,7 +459,7 @@ WorkerThread::getNumWorkers() const {
 
 inline bool
 WorkerThread::shouldStop(int jobId) const {
-    return (this->jobId != jobId) || terminate;
+    return this->jobId != jobId;
 }
 
 inline void

texellib/src/computerPlayer.cpp
@@ -41,7 +41,7 @@ static StaticInitializer<ComputerPlayer> cpInit;
 
 void
 ComputerPlayer::staticInitialize() {
-    std::string name = "Texel 1.07";
+    std::string name = "Texel 1.08a1";
     if (sizeof(char*) == 4)
         name += " 32-bit";
     engineName = name;

commit e6f9ab48c50f698141127f8ed557091222ae8925

Texel: Fixed deadlock when reducing number of search threads.
Caused by 71512d3d.

texellib/src/parallel.cpp
@@ -554,7 +554,7 @@ ThreadCommunicator::notifyThread() {
 
 WorkerThread::WorkerThread(int threadNo, Communicator* parentComm,
                            int numWorkers, TranspositionTable& tt)
-    : threadNo(threadNo), numWorkers(numWorkers), tt(tt) {
+    : threadNo(threadNo), numWorkers(numWorkers), terminate(false), tt(tt) {
     if (parentComm) {
         auto f = [this,parentComm]() {
             mainLoop(parentComm, false);
@@ -565,6 +565,7 @@ WorkerThread::WorkerThread(int threadNo, Communicator* parentComm,
 
 WorkerThread::~WorkerThread() {
     children.clear();
+    terminate = true;
     threadNotifier.notify();
     if (thread)
         thread->join();
@@ -626,6 +627,8 @@ WorkerThread::mainLoop(Communicator* parentComm, bool cluster) {
     while (true) {
         bool handleClusterComm = Cluster::instance().isEnabled() && threadNo == 0;
         threadNotifier.wait(handleClusterComm ? 1 : -1);
+        if (terminate)
+            break;
         comm->poll(handler);
         if (comm->hasQuitAck())
             break;

texellib/src/parallel.hpp
@@ -401,6 +401,7 @@ private:
     const int numWorkers; // Number of worker threads including all child threads
 
     Notifier initialized;
+    std::atomic<bool> terminate;
 
     std::unique_ptr<Evaluate::EvalHashTables> et;
     std::unique_ptr<KillerTable> kt;