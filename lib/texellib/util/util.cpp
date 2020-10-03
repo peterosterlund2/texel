272d41d55bd22554b8cd674161094a27af6407b1
Peter Osterlund <peterosterlund2@gmail.com>
2020-10-03 14:47:50 +0200

    Don't allow compilation if sizeof(int) is too small
    The source code assumes ints are at least 32 bits large.

--- lib/texellib/util/util.cpp
+++ lib/texellib/util/util.cpp
@@ -25,6 +25,8 @@
 
 #include "util.hpp"
 
+static_assert(sizeof(int) >= sizeof(S32), "sizeof(int) too small");
+
 static const int firstBitTable[32] = {
     0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
     8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
